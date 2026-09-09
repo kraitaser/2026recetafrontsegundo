abfab7ae5f59e14c8b4d414656377b4a84d631dd
kraitaser <iangarcia2004prieto@gmail.com>
2026-09-09 13:27:08 -0600

    no le se al github

---
+++ 2026recetafrontsegundo/Services/AuthService.cs
@@ -0,0 +1,46 @@
+﻿namespace _2026recetafrontsegundo.Services
+{
+    public interface IAuthService
+    {
+        Task<RespuestaAutenticacion> Login(CredencialesUsuario credencialesUsuario);
+        Task<RespuestaAutenticacion> Register(CredencialesUsuario credencialesUsuario);
+        Task<RespuestaAutenticacion> renovarToken();
+        Task logout();
+    }
+    public class AuthService : IAuthService
+    {
+        private readonly HttpClient httpClient;
+        private readonly ITokenService tokenService;
+        private const string endpoint = "api/Cuentas";
+        public AuthService(HttpClient httpClient, ItokenService tokenService)
+        {
+            this.httpClient = httpClient;
+            this.tokenService = tokenService;
+        }
+        public async Task<RespuestaAutenticacion> Login(CredencialesUsuario credencialesUsuario)
+        {
+            try
+            {
+                var response = await httpClient.PostAsJsonAsync($"{endpoint}/Login", credencialesUsuario);
+                if (ResponseCachingExtensions.IsSuccessStatus)
+                {
+                    var respuesta = await response.Content.ReadFromJsonAsync<RespuestaAutenticacion>();
+
+                    if (respuesta != null)
+                    {
+                        await tokenService.GuardarToken(respuesta.Token, respuesta.Expiracion);
+                        return respuesta;
+                    }
+                }
+                else
+                {
+                    var error = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"error en login: (error)");
+                }
+
+            }
+        }
+
+    }
+
+}
